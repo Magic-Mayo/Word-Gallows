fc171b36df0746f998ba2a1a58f1616289a35b5c
MagicMayo <mike.mayoii@gmail.com>
2019-05-22 15:39:00 -0700

    created function to add name to body of html after prompted. changed some text colors and size

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,20 @@
 alert("Beware: Henceforth erroneous suppositions shall be tallied in regards to thy hanging by the neck.  Should thy tally reach five, you shall be hanged!");
 
-const name = prompt("T'is thy name?");
+confirm("Doest thou wish to proceed?")
 
-if (true) {
-    document.getElementById("name").insertAdjacentText("afterbegin", name);
+if (confirm) {
+    const name = prompt("T'is thy name?");
+    function yourName() {
+        const userName = document.getElementById("name");
+        const writeName = document.createTextNode(name);
+        userName.appendChild(writeName);
+        console.log(writeName);
+    }
+    
+}
+
+
+else {
+
+
 }
