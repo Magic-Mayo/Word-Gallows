e89439349f5f10dab8423a0bbfbe120f2c447b15
Magic-Mayo <mike.mayoii@gmail.com>
2019-05-22 02:44:01 -0700

    added background image. started guess countdown in index.html. started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+alert("Beware: Henceforth erroneous suppositions shall be tallied in regards to thy hanging by the neck.  Should thy tally reach five, you shall be hanged!");
+
+const name = prompt("T'is thy name?");
+
+if (true) {
+    document.getElementById("name").insertAdjacentText("afterbegin", name);
+}
