d64948cf6108bce9052b9ae0e2dd68776d3bdf6b
Magic-Mayo <mike.mayoii@gmail.com>
2019-05-23 00:01:01 -0700

    made else statement to execute function to change bg if user decides to not play when prompted

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-alert("Beware: Henceforth erroneous suppositions shall be tallied in regards to thy hanging by the neck.  Should thy tally reach five, you shall be hanged!");
+const warn = confirm("Beware: Henceforth erroneous suppositions shall be tallied in regards to thy hanging by the neck.  Should thy tally reach five, you shall be hanged!  Doest thou wish to proceed?");
 
-confirm("Doest thou wish to proceed?")
+// confirm("Doest thou wish to proceed?");
 
-if (confirm) {
+if (warn) {
     const name = prompt("T'is thy name?");
 
     // Function to add name to body of HTML after prompted
@@ -12,12 +12,19 @@ if (confirm) {
         userName.appendChild(writeName);
         console.log(writeName);
     }
+
+    // Executes function after document is loaded
+    window.addEventListener('DOMContentLoaded', yourName)
+}
+
+// If user chooses not to play they will be given this message and sent to a separate page
+else if (!warn){
+    alert("Thou hast chosen to be but a coward.  But if nothing else thy neck shan't be broken this day.");
+    console.log(false);
+
+    function cowardBG(cowardImage) {
+        document.getElementById("body").className = "bg" + cowardImage;
+    }
     
-}
-
-else {
-
 
 }
-
-onkeypress.
