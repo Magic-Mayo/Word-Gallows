154671505ceecfaf5f43ded130c75984a4122a4a
Magic-Mayo <mike.mayoii@gmail.com>
2019-05-26 16:08:32 -0700

    added more content to html, changed warn conditional to allow only 3 guesses if user tries to opt out of playing, added some styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,34 @@
-const warn = confirm("Beware: Henceforth erroneous suppositions shall be tallied in regards to thy hanging by the neck.  Should thy tally reach five, you shall be hanged!  Doest thou wish to proceed?");
+// const warn = confirm("Beware: Henceforth erroneous suppositions shall be tallied in regards to thy hanging by the neck.  Should thy tally reach five, you shall be hanged!  Doest thou wish to proceed?");
 
-// confirm("Doest thou wish to proceed?");
+// if (warn) {
+//     const name = prompt("T'is thy name?");
 
-if (warn) {
-    const name = prompt("T'is thy name?");
+//     // Function to add name to body of HTML after prompted
+//     function yourName() {
+//         const userName = document.getElementById("name");
+//         const writeName = document.createTextNode(name);
+//         userName.appendChild(writeName);
+//         console.log(writeName);
+//     }
 
-    // Function to add name to body of HTML after prompted
-    function yourName() {
-        const userName = document.getElementById("name");
-        const writeName = document.createTextNode(name);
-        userName.appendChild(writeName);
-        console.log(writeName);
-    }
+//     // Executes yourName function after document is loaded
+//     window.addEventListener('DOMContentLoaded', yourName)
+// }
 
-    // Executes yourName function after document is loaded
-    window.addEventListener('DOMContentLoaded', yourName)
-}
+// // If user chooses not to play they will be given this message and forced to play with less chance to guess wrong
+// else if (!warn) {
+//     alert("Thou hast chosen to be but a coward.  For thine treachery, ye shall be given only three faulty predictions!");
+//     const coward = prompt("T'is thy name, coward?");
 
-// If user chooses not to play they will be given this message and display a different background
-else if (!warn) {
-    alert("Thou hast chosen to be but a coward.  But if nothing else thy neck shan't be broken this day.");
+//         // Function to add name to body of HTML after prompted
+//         function cowardName() {
+//             const cowardName = document.getElementById("name");
+//             const writeCoward = document.createTextNode(coward);
+//             cowardName.appendChild(writeCoward);
+//             console.log(writeCoward);
+//         }
+        
+//         // Executes coward function after document is loaded
+//         window.addEventListener('DOMContentLoaded', cowardName);
+// }
 
-    function cowardBG(cowardImage) {
-        document.getElementById("body").className = "bg" + cowardImage;
-        const container = document.getElementByClassName("para");
-        container.style.display = "none";
-    }
-
-
-}
