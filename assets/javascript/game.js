48d49fc9ada8a7c804a4e092264028780f3431a9
MagicMayo <mike.mayoii@gmail.com>
2019-05-29 16:41:31 -0700

    changing event key function to begin function so math.random can be ran in the local scope so it can choose a new word at random when the game is over. commented out restart function and moved some of its statements to start function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,9 +33,9 @@ const words = [
 // console.log(words.length);
 
 
-// console.log(wordChoice);
 // const userPress = document.getElementById("guess");
 let wordChoiceLength = [];
+// const wordChoice = words[Math.floor(Math.random() * words.length)];
 
 let wins = [0];
 document.getElementById('saved').textContent = wins;
@@ -44,67 +44,87 @@ document.getElementById('broken').textContent = losses;
 let guesses = [5];
 document.getElementById('turn').textContent = guesses;
 
-start();
+// start();
+// console.log(wordChoice);
 
 // Function to determine if a key event correctly identifies a letter in the randomly chosen word
-document.onkeyup = function (event) {
-    
-    // start();
-    
+function begin() {
     const wordChoice = words[Math.floor(Math.random() * words.length)];
-    const letter = document.getElementById('word').textContent.split(' ');
-    const charCode = event.which;
-    console.log(wordChoice)
-    if ((charCode > 64 && charCode < 91)) {
-        for (let i = 0; i < wordChoice.length; i++) {
-            if (event.key === wordChoice.charAt(i)) {
-                letter[i] = event.key;
-                document.getElementById('word').textContent = letter.join(' ');
-                // console.log(document.getElementById('word').textContent.includes('_'))
-                if (!document.getElementById('word').textContent.includes('_')) {
-                    restart();
+    document.onkeyup = function (event) {
+
+        start();
+        // const wordChoice = words[Math.floor(Math.random() * words.length)];
+        // const word = wordChoice;
+        const letter = document.getElementById('word').textContent.split(' ');
+        const charCode = event.which;
+        console.log(wordChoice);
+        if ((charCode > 64 && charCode < 91)) {
+            for (let i = 0; i < wordChoice.length; i++) {
+                if (event.key === wordChoice.charAt(i)) {
+                    letter[i] = event.key;
+                    document.getElementById('word').textContent = letter.join(' ');
+                    // console.log(document.getElementById('word').textContent.includes('_'))
+                    if (!document.getElementById('word').textContent.includes('_')) {
+                        start();
+                    }
                 }
+                // while (!document.getElementById('word').textContent.includes('_')) {
+                //     randomWord(23);
+                // }
             }
-            // while (!document.getElementById('word').textContent.includes('_')) {
-            //     randomWord(23);
-            // }
-        }
-        while (! wordChoice.includes(event.key)) {
-            document.getElementById('guess').textContent += event.key + ' ';
-            guesses[0] = document.getElementById('turn').textContent -= 1;
-            if (document.getElementById('turn').textContent === 0) {
-                const hang = new Audio('assets/trap door.mp3');
-                const bone = new Audio('assets/bone.mp3');
-                const scream = new Audio('assets/scream.mp3');
-                hang.play();
-                setTimeout(function () {bone.play();}, 700);
-                setTimeout(function () {scream.play();}, 800);
-                // scream.play();
-                console.log(document.getElementById('turn'));
-                document.getElementById('broken').textContent += 1;
-                restart();
-            }
+            if (! wordChoice.includes(event.key)) {
+                document.getElementById('guess').textContent += event.key + ' ';
+                guesses[0] = document.getElementById('turn').textContent -= 1;
+                if (document.getElementById('turn').textContent == 0) {
+                    const hang = new Audio('assets/trap door.mp3');
+                    const bone = new Audio('assets/bone.mp3');
+                    const scream = new Audio('assets/scream.mp3');
+                    hang.play();
+                    setTimeout(function () {
+                        bone.play();
+                    }, 700);
+                    setTimeout(function () {
+                        scream.play();
+                    }, 800);
+                    // scream.play();
+                    console.log(document.getElementById('turn'));
+                    document.getElementById('broken').textContent += 1;
+                    begin();
+                }
 
 
+            }
+            // console.log(event.which)
         }
-        // console.log(event.which)
     }
 }
 
-function start() {
+begin();
+
+function start() { // nextWord(wordChoice);
     const wordChoice = words[Math.floor(Math.random() * words.length)];
     for (let i = 0; i < wordChoice.length; i++) {
         wordChoiceLength[i] = '_';
         document.getElementById('word').innerHTML = wordChoiceLength.join(' ');
-        
+    }
+    if (document.getElementById('word').textContent = wordChoice) {
+        document.getElementById('turn').textContent = 5;
+        document.getElementById('guess').textContent = null;
+        console.log(wordChoice);
+
     }
 }
 
-function restart() {
-    const wordChoice = words[Math.floor(Math.random() * words.length)];
-    if (document.getElementById('word').textContent = wordChoice) {
-        start();
-        document.getElementById('turn').textContent = 5;
-        console.log(wordChoice);
-    }
-}
+// function restart() {
+//     const wordChoice = words[Math.floor(Math.random() * words.length)];
+
+//     if (document.getElementById('word').textContent = wordChoice) {
+//         start();
+//         document.getElementById('turn').textContent = 5;
+//         document.getElementById('guess').textContent = null;
+//         console.log(wordChoice);
+//     }
+// }
+
+// let nextWord = new function (a)
+//     a = wordChoice
