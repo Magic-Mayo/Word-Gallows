07d15d2084660525c20793611dbefd35c0a306d0
MagicMayo <mike.mayoii@gmail.com>
2019-06-03 20:40:54 -0700

    moved blocks of code around so game would function correctly. used parseint so it would add to the win and loss scores appropriately. changed wins and losses variables back to numbers from null. added some comments to js and deleted some comments from html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// Words for computer to choose from for user to guess
+// Words for computer to choose from randomly for user to guess
 chooseWord = function () {
     let wordChoiceLength = [];
     words = [
@@ -27,71 +27,53 @@ chooseWord = function () {
         'guillotine'
     ];
     wordChoice = words[Math.floor(Math.random() * words.length)];
- 
+
     for (let i = 0; i < wordChoice.length; i++) {
-    wordChoiceLength[i] = '_';
-    document.getElementById('word').innerHTML = wordChoiceLength.join(' ');
+        wordChoiceLength[i] = '_';
     }
+    if (document.getElementById('word').textContent = wordChoiceLength) {
+        document.getElementById('turn').textContent = 5;
+        document.getElementById('guess').textContent = null;
+    }
+
+    document.getElementById('word').innerHTML = wordChoiceLength.join(' ');
+    console.log(wordChoiceLength);
     console.log(wordChoice);
 
-    if (document.getElementById('word').textContent = wordChoice) {
-    document.getElementById('turn').textContent = 5;
-    document.getElementById('guess').textContent = null;
-
-    }
 }
-// Random word choice
 
-// function randomWord(max) {
-//     return wordChoice;
-// }
-// console.log(words.length);
-
-
-// const userPress = document.getElementById("guess");
-// let wordChoiceLength = [];
-// const wordChoice = words[Math.floor(Math.random() * words.length)];
-
-let wins = [null];
+let wins = [0];
 document.getElementById('saved').textContent = wins;
-let losses = [null];
+let losses = [0];
 document.getElementById('broken').textContent = losses;
 let guesses = [5];
 document.getElementById('turn').textContent = guesses;
-
-// start();
-// console.log(wordChoice);
 
 // Function to determine if a key event correctly identifies a letter in the randomly chosen word
 function begin() {
     chooseWord();
     document.onkeyup = function (event) {
 
-        // start();
-        // const wordChoice = words[Math.floor(Math.random() * words.length)];
-        // const word = wordChoice;
         const letter = document.getElementById('word').textContent.split(' ');
         const charCode = event.which;
-        // console.log(wordChoice);
+
         if ((charCode > 64 && charCode < 91)) {
             for (let i = 0; i < wordChoice.length; i++) {
                 if (event.key === wordChoice.charAt(i)) {
                     letter[i] = event.key;
                     document.getElementById('word').textContent = letter.join(' ');
-                    // console.log(document.getElementById('word').textContent.includes('_'))
                     if (!document.getElementById('word').textContent.includes('_')) {
-                        // start();
                         chooseWord;
                     }
                 }
-                // while (!document.getElementById('word').textContent.includes('_')) {
-                //     randomWord(23);
-                // }
             }
-            if (! wordChoice.includes(event.key)) {
+
+            if (!wordChoice.includes(event.key)) {
                 document.getElementById('guess').textContent += event.key + ' ';
                 guesses[0] = document.getElementById('turn').textContent -= 1;
-                if (document.getElementById('turn').textContent == 0) {
+
+                // Determines if a game is lost and tallies the score in the necks broken column
+                gameOver = function () {
                     const hang = new Audio('assets/trap door.mp3');
                     const bone = new Audio('assets/bone.mp3');
                     const scream = new Audio('assets/scream.mp3');
@@ -102,45 +84,30 @@ function begin() {
                     setTimeout(function () {
                         scream.play();
                     }, 800);
-                    // scream.play();
                     console.log(document.getElementById('turn'));
-                    document.getElementById('broken').textContent += 1;
+                    document.getElementById('broken').textContent = parseInt(document.getElementById('broken').textContent) + 1;
+                }
+
+                // Determines if a game is won and tallies the score in necks saved 
+                gameWin = function(){
+                    document.getElementById('saved').textContent = parseInt(document.getElementById('saved').textContent) + 1;
                     begin();
                 }
 
+                if (!document.getElementById('word').textContent.includes('_')) { 
+                    gameWin();
+                    begin();
+                }
 
+                if (document.getElementById('turn').textContent == 0) {
+                    gameOver();
+                    begin();
+                }
+                console.log(document.getElementById('word').textContent)
             }
-            // console.log(event.which)
         }
     }
 }
 
+
 begin();
-
-// function start() { // nextWord(wordChoice);
-//     const wordChoice = words[Math.floor(Math.random() * words.length)];
-//     for (let i = 0; i < wordChoice.length; i++) {
-//         wordChoiceLength[i] = '_';
-//         document.getElementById('word').innerHTML = wordChoiceLength.join(' ');
-//     }
-//     if (document.getElementById('word').textContent = wordChoice) {
-//         document.getElementById('turn').textContent = 5;
-//         document.getElementById('guess').textContent = null;
-//         console.log(wordChoice);
-
-//     }
-// }
-
-// function restart() {
-//     const wordChoice = words[Math.floor(Math.random() * words.length)];
-
-//     if (document.getElementById('word').textContent = wordChoice) {
-//         start();
-//         document.getElementById('turn').textContent = 5;
-//         document.getElementById('guess').textContent = null;
-//         console.log(wordChoice);
-//     }
-// }
-
-// let nextWord = new function (a)
-//     a = wordChoice
